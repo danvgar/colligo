3c548cd14ccd7e03c7c6bd8bd750aee863aa4f7a
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-01-23 01:35:30 -0800

    Various minor adjustments to login, auth, and installed packages such as zod, dotenv, and NextAuth (that I am now reading I will regret later)

--- README.md
+++ README.md
@@ -41,3 +41,6 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 - Shadcn
 - HeroIcons
 - Vercel Postgres SDK
+- dotenv
+- NextAuth
+- zod
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import ColligoLogo from '@/ui/colligo-logo';
-// import LoginForm from '@/app/ui/login-form';
+import LoginForm from '@/ui/login-form';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -10,7 +10,7 @@ export default function LoginPage() {
     return (
         <main className="flex items-center justify-center md:h-screen">
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-                <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+                <div className="flex h-20 w-full items-end rounded-lg bg-black-500 p-3 md:h-36">
                     <div className="w-32 text-white md:w-36">
                         <ColligoLogo />
                     </div>
