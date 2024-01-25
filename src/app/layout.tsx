9e2bec0c93f5fe408edddc4f99410e467a1973c5
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-01-24 17:22:34 -0800

    Apparently when using Mantine with NextJS, theme will render differently locally vs deploy.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import '@mantine/core/styles.css';
 
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
