0496be4eb97d3ce2700c49467574b50847a5bbf6
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-01-27 17:11:50 -0800

    Removed LightDarkToggle as those are no longer used. Changed Home component to App in hopes that _document would somehow read it. But also changed _document component to dark, which we've already seen doesn't do anything meaningful.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 import LinkCard from "@/components/linkcard";
 
-export default function Home() {
+export default function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <LinkCard />
