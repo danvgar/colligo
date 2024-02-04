147ee8b8e93c2cd8c2486c7398c2c4a121e40ccf
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-02-03 21:16:16 -0800

    Added table component for links

---
+++ src/components/table.tsx
@@ -0,0 +1,124 @@
+import Image from 'next/image';
+import { UpdateLink, DeleteLink } from '@/components/buttons-backend';
+// import LinkStatus from '@/components/links/status';
+// import { formatDateToLocal, formatCurrency } from '@/lib/utils';
+import { fetchFilteredLinks } from '@/lib/data';
+
+export default async function LinksTable({
+  query,
+  currentPage,
+}: {
+  query: string;
+  currentPage: number;
+}) {
+  const links = await fetchFilteredLinks(query, currentPage);
+
+  return (
+    <div className="mt-6 flow-root">
+      <div className="inline-block min-w-full align-middle">
+        <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+          <div className="md:hidden">
+            {links?.map((link) => (
+              <div
+                key={link.id}
+                className="mb-2 w-full rounded-md bg-white p-4"
+              >
+                <div className="flex items-center justify-between border-b pb-4">
+                  <div>
+                    <div className="mb-2 flex items-center">
+                      <Image
+                        src={link.image_url}
+                        className="mr-2 rounded-full"
+                        width={28}
+                        height={28}
+                        alt={`${link.name}'s profile picture`}
+                      />
+                      <p>{link.name}</p>
+                    </div>
+                    <p className="text-sm text-gray-500">{link.email}</p>
+                  </div>
+                  <LinkStatus status={link.status} />
+                </div>
+                <div className="flex w-full items-center justify-between pt-4">
+                  <div>
+                    <p className="text-xl font-medium">
+                      {formatCurrency(link.amount)}
+                    </p>
+                    <p>{formatDateToLocal(link.date)}</p>
+                  </div>
+                  <div className="flex justify-end gap-2">
+                    <UpdateLink id={link.id} />
+                    <DeleteLink id={link.id} />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <table className="hidden min-w-full text-gray-900 md:table">
+            <thead className="rounded-lg text-left text-sm font-normal">
+              <tr>
+                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                  Customer
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Email
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Amount
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Date
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Status
+                </th>
+                <th scope="col" className="relative py-3 pl-6 pr-3">
+                  <span className="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white">
+              {links?.map((link) => (
+                <tr
+                  key={link.id}
+                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                >
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex items-center gap-3">
+                      <Image
+                        src={link.image_url}
+                        className="rounded-full"
+                        width={28}
+                        height={28}
+                        alt={`${link.name}'s profile picture`}
+                      />
+                      <p>{link.name}</p>
+                    </div>
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {link.email}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {formatCurrency(link.amount)}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {formatDateToLocal(link.date)}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    <LinkStatus status={link.status} />
+                  </td>
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex justify-end gap-3">
+                      <UpdateLink id={link.id} />
+                      <DeleteLink id={link.id} />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
