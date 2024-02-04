e20955dcfb47d2a01a2896a8387a041d0cc9e1e6
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-02-03 20:35:41 -0800

    Last commit before crazy stuff.
    Renamed button components for clarity. Code has gotten a bit hard to manage so going to try something else.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 // import Image from "next/image";
 import LinkCard from "@/components/linkcard";
 import Search from "@/components/search";
-import { CreateLink } from "@/components/buttons"
+import { CreateLink } from "@/components/buttons-backend"
 
-export default function Home() {
+export default function Page(
+// We are going to add searchParams into the dynamic route, such as an array of tags. 
+) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
