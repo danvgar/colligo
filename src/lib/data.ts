e49a3b4d98430262a06926b789f0c7366dfcac4b
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-02-03 21:04:20 -0800

    Just kidding, I haven't given up yet. Added fetchFilteredLinks database query from server side.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,13 +1,51 @@
 import { sql } from '@vercel/postgres';
-import { Link } from './definitions';
+import { Link, LinksTable } from './definitions';
+import { unstable_noStore as noStore } from 'next/cache';
 
-export async function fetchLinks() {
+export async function fetchLatestLinks() {
   try {
     const links = await sql<Link>`SELECT * FROM links`;
 
     console.log('Links:', JSON.stringify(links.rows, null, 2));
     return links.rows;
 
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch links.');
+  }
+}
+
+const ITEMS_PER_PAGE = 6;
+export async function fetchFilteredLinks(
+  query: string,
+  currentPage: number,
+) {
+  noStore();
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
+  try {
+    const links = await sql<LinksTable>`
+      SELECT
+        links.id,
+        links.title,
+        links.url,
+        links.tags,
+        links.description,
+        links.dateAdded,
+        links.votes
+      FROM links
+      WHERE
+        links.id ILIKE ${`%${query}%`} OR
+        links.title ILIKE ${`%${query}%`} OR
+        links.url ILIKE ${`%${query}%`} OR
+        links.tags ILIKE ANY (ARRAY[${`%${query}%`}]) OR
+        links.description ILIKE ${`%${query}%`} OR
+        links.dateAdded::text ILIKE ${`%${query}%`} OR
+        links.votes::text ILIKE ${`%${query}%`} OR
+      ORDER BY links.title DESC
+      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
+    `;
+
+    return links.rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch links.');
--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -24,3 +24,14 @@ export type Link = {
   dateAdded: string;
   votes: number;
 };
+
+export type LinksTable = {
+  id: string;
+  customer_id: string;
+  name: string;
+  email: string;
+  image_url: string;
+  date: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
