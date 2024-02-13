8ca4131151e92c619193944d60a23d6f4d2b2e9e
Daniel Garcia <124107401+danvgar@users.noreply.github.com>
2024-02-12 21:33:03 -0800

    Added Zod form validation. Still have error with how string arrays are validated and stored with SQL. Need to continue investigating

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,7 +1,3 @@
-import { sql } from '@vercel/postgres';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
-
 // To-Do
 // ********************
 //  - Form Validation
@@ -9,22 +5,52 @@ import { redirect } from 'next/navigation';
 //  - Add list functionality
 
 
+// Dependencies
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+
+// Zod Form Validation
+const FormSchema = z.object({
+    // listID: z.string(),
+    id: z.string(),
+    url: z.string().url(),
+    title: z.string().max(15),
+    desc: z.string().max(30),
+    tags: z.string(), // Consider using enum with an array of existing tags. 
+    date: z.string(),
+})
+
+// Schema Variation: Ignore id and date validation.
+const CreateLink = FormSchema.omit({ id: true, date: true })
+
 // Create Links
 // ********************
-
 export async function createLink(formData: FormData) {
     'use server';
 
-    // Prepare data for insertion into the database
-    const date = new Date().toISOString().split('T')[0];
 
-    const rawFormData = {
+    // If formData passes Zod validation parsing, toss it into destructured object variables.
+    const { url, title, desc, tags } = CreateLink.parse({
         // listID: formData.get('listID'),
         url: formData.get('url'),
         title: formData.get('title'),
         desc: formData.get('desc'),
-        tags: formData.get('tags'),
-    };
+        tags: formData.get('tags'), // Currently broken
+    })
+
+    // Create date for time link was created
+    const date = new Date().toISOString().split('T')[0];
+
+    // Split tags string into comma separated, trimmed elements of an array.
+    const tagsArr: string[] = tags.split(",").map(tag => tag.trim());
+
+    await sql`
+    INSERT INTO links (url, title, description, tags)
+    VALUES (${url}, ${title}, ${desc}, ARRAY[${tagsArr.map(tag => sql.tag`${tag}`).join(',')}])
+  `;
 
     // Revalidate the cache for the links page and redirect the user.
     revalidatePath('/');
